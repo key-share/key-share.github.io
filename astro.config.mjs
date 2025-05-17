c532464943c04eedee49c26e55847bd3d4df387d
key-share <2748774135@qq.com>
2025-05-17 18:19:55 +0800

    测试

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+// import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+// export default defineConfig({
+
+// });
+
+import rehypeWrap from 'rehype-wrap';
+import rehype-slug from 'rehypePlugins'
+
+export default {
+  markdown: {
+    rehypePlugins: 
+    [
+      [rehypeWrap, 
+        {
+        selector: 'h1, h2, h3, h4, h5, h6',
+        multiple: true,
+        wrapper: 'div.heading-wrapper',
+        }],
+      'rehype-slug',
+      'rehype-autolink'  
+    ]
+  }
+}
